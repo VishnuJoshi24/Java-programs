ddd956dc5b4e3a08d56d12e9000b0eb2b8133ffe
VishnuJoshi24 <167234750+VishnuJoshi24@users.noreply.github.com>
2025-08-04 22:57:33 +0530

    Add files via upload

---
+++ CheckPositive.java
@@ -0,0 +1,9 @@
+public class CheckPositive {
+    public static void main(String[] args) {
+        int num = 5;
+        if (num > 0)
+            System.out.println("Positive number.");
+        else
+            System.out.println("Not a positive number.");
+    }
+}
---
+++ CompareNumbers.java
@@ -0,0 +1,11 @@
+public class CompareNumbers {
+    public static void main(String[] args) {
+        int n1 = 10, n2 = 20;
+        if (n1 > n2)
+            System.out.println("n1 is greater");
+        else if (n1 < n2)
+            System.out.println("n2 is greater");
+        else
+            System.out.println("Both are equal");
+    }
+}
